b99ac1b56b83f3b208a7d2d69ff51990a401d985
Caeli Rice <CRice@madisoncollege.edu>
2020-04-16 22:15:12 -0500

    Add project files.

---
+++ ResumePortfolioSite/Views/Resume/Education.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResumePortfolioSite.Views.Resume
+{
+    public class Education
+    {
+        public int EducationId { get; set; }
+        public string SchoolName { get; set; }
+        public int StartYear { get; set; }
+        public string StartMonth { get; set; }
+        public int GraduationYear { get; set; }
+        public string GraduationMonth { get; set; }
+
+    }
+}
